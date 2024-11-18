9ca73235f08c54cf86dc35fa5cb499251a3465aa
deepesh2301 <deepeshsahu9840@gmail.com>
2024-11-18 20:31:15 +0530

    First Commit Push all project

---
+++ Promt 3/app.js
@@ -0,0 +1,13 @@
+// let x=+prompt("Enter first number");
+// let y = +prompt("Enter sec number");
+// let z = +prompt("Enter thired number");
+
+// if (x==y && y == z){
+//     console.log("all are equal");
+// }
+// else if(x==y || y==z || z==x){
+//     console.log("two are equal");
+// }
+// else {
+//     console.log("none are equal")
+// }
